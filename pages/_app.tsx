6d34ebf4ae7b41729487b311fa52db12c3f62760
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-03-07 05:03:43 +0000

    "Initial commit"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
+import { DAppProvider } from "@usedapp/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+   <DAppProvider config={{}}>
+    <Component {...pageProps} />
+   </DAppProvider>)
 }
