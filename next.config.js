dcb84c6440515fb2f840731f668a820e8a484ba0
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-03-12 07:56:17 +0000

    "Initial commit"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+    reactStrictMode: true,
+  }
 }
-
-module.exports = nextConfig
--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,1 +1,12 @@
-export {}
+import styled from 'styled-components'
+import { Flex } from '@chakra-ui/react'
+
+function Create() {
+    return (
+        <Flex>
+            <a>h</a>
+        </Flex>
+    )
+}
+
+export default Create
